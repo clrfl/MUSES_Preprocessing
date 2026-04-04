a6dd54f624d401405200394056544e930068c6f4
Moritz <60201269+clrfl@users.noreply.github.com>
2026-04-04 13:29:16 +0200

    Update step2_loader.py

--- spiketrains/step2_loader.py
+++ spiketrains/step2_loader.py
@@ -14,11 +14,6 @@ for idx in [1,2,3,4,5,6]:
 
         dfs = pd.DataFrame(S)
 
-        #dfs=dfs.head(10000)
-        #
-        # plt.plot(dfs.index, dfs[0])
-        # plt.show()
-
         with open("full_preproc_" + str(idx) + ".jsonl", 'w', encoding='utf-8') as g:
 
             for x in dfs.columns:
@@ -31,5 +26,3 @@ for idx in [1,2,3,4,5,6]:
                 new_df['seconds'] = new_df.index / 50
 
                 g.write(new_df.T.to_json(orient="values") + '\n')
-
-                #print(new_df)
